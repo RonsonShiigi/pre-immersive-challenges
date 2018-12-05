b1706288389f5f8b5ded8c16e4c197e34f594ebf
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-12-05 12:47:10 -1000

    pau problem

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -2,14 +2,16 @@ module.exports = function cookieProblem(array) {
   // write your code in here
 var max = Math.max.apply(null, array);
 console.log(max);
-
+var total= 0
 for(var i=0; i<array.length;i++){
-var extra = max-array[i];
-console.log(extra);
+  
+ total += (max-array[i]);
+console.log(total);
 
 
 
 }
+return total;
 
 
   
