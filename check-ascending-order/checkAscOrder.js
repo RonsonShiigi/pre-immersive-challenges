4aafb00cc1a8f4e7761b9ddbd1f9d575c11d0669
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-12-05 11:51:09 -1000

    pau check order

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,35 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
+  var ho =  numArray;
+for(var i=0; i<ho.length;i++){
+  // if(ho[i] < ho[i-1]){if(ho[i+1] < ho[i]){
+  //   return(false);
+  // }
+    if(ho[i] < ho[i+1]){
+      return(true)
+    }
+    if(ho[i]> ho[i+1]){
+      return(false)
+    }
+  
+  
+  
+  
+  }
+  
+  
+
 }
+
+  
+  
+  
+  
+
+ 
+  
+
+
+
+
+
