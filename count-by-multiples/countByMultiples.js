7c9bd4c9696d0f591da53ff3dc8b562c69f0169b
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-12-05 13:04:02 -1000

    pau EZAY

--- count-by-multiples/countByMultiples.js
+++ count-by-multiples/countByMultiples.js
@@ -1,3 +1,14 @@
 module.exports = function countByMultiples(length, x) {
   // write your code in here
+  // console.log(length)
+  // console.log(x)
+  var add = 0;
+  var arr = [];
+  for(var i=0;i<length;i++){
+     add += x;
+     arr.push(add);
+    
+  }
+  return arr;
+
 }
