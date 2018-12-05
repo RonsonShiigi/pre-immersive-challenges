fc651322b727eb51d4b2782f17306e3df6d58906
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-12-05 13:48:14 -1000

    got integer into array

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -2,14 +2,12 @@ module.exports = function descendingOrder(number) {
   // write code in here
  var arr = [];
 if(Number.isInteger(number)){
-  // console.log('hi');
-  //TYPE IN DESCENDING ORDER
-  var long = Math.max(Math.floor(Math.log10(Math.abs(number))), 0) + 1;
+  // console.log(number);
+  var digits = (""+number).split("");
+  console.log(digits)
   
-  for(var i=0; i<long.length; i++){
-  
-  }
-console.log(long);
+
+ 
 
 }else{
   return ('not a number!')
