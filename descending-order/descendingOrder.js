03a02867baf75d0db64e90ed6e63e813a41e7852
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-12-05 13:36:57 -1000

    working on for loop

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,9 +1,20 @@
 module.exports = function descendingOrder(number) {
   // write code in here
- 
+ var arr = [];
 if(Number.isInteger(number)){
-  console.log('hi');
+  // console.log('hi');
+  //TYPE IN DESCENDING ORDER
+  var long = Math.max(Math.floor(Math.log10(Math.abs(number))), 0) + 1;
+  
+  for(var i=0; i<long.length; i++){
+  
+  }
+console.log(long);
+
+}else{
+  return ('not a number!')
 }
+
 }
 
   
