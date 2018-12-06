8916772daa825abffe9a980672b0342a98b2ecf1
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-12-05 20:00:57 -1000

    next condition done, accessed string and double letters, must put in new string

--- double-str-chars/doubleStrChars.js
+++ double-str-chars/doubleStrChars.js
@@ -1,6 +1,18 @@
 module.exports = function doubleStrChars(str) {
   var type = typeof str;
-  if(type != 'string'){
-    return "not a string!"
+  if(type === 'string'){
+    // console.log('boom')
+    for(var i=0; i<str.length;i++){
+    //  console.log(str.charAt(i));
+    var double = str.charAt(i)+str.charAt(i);
+    console.log(double)
+    }
+  }
+  
+  
+  
+  
+  else{
+    return 'not a string!'
   }
 }
