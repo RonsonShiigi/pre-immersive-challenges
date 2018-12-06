41a5ab13bcf861bb3bdc63f782e8e82bc5fd3655
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-12-05 14:38:01 -1000

    stuck, cant get array into number

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,10 +1,20 @@
 module.exports = function descendingOrder(number) {
   // write code in here
- var arr = [];
+
 if(Number.isInteger(number)){
   // console.log(number);
-  var digits = (""+number).split("");
-  console.log(digits)
+  var arr = (""+number).split("");
+  // console.log(digits)
+  var newArr =  arr.sort(function(a, b){return b-a});
+  var string = newArr.toString();
+  var number = parseInt(string);
+  // console.log(newNum);
+for(var i=0; i<newArr.length; i++){
+  var st = newArr[i].toString();
+  var newSt= st + st[i]
+  console.log(newSt)
+
+}
   
 
  
